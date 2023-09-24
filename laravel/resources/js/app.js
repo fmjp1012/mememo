75420cd7135cba0dc07f3941b529a60f143abacb
fmjp1012 <fmjp1012@gmail.com>
2023-09-24 21:49:51 +0900

    vue, viteを導入

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+import "./bootstrap";
+import "../css/app.css";
+
+//vue & route
+import { createApp } from "vue";
+
+//component
+// import TopIndex from "./componen/Top/TopIndex.vue";
+import Test from "./components/Test.vue";
+
+
+const app = createApp({});
+app.component("Test", Test);
+
+app.mount("#app");
