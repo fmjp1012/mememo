40a972cde1c9ad44bc68816de975e214b87ad5eb
fmjp1012 <fmjp1012@gmail.com>
2023-09-25 12:03:01 +0900

    vite環境をdockerコンテナ上で実現

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -14,17 +14,7 @@ export default defineConfig({
         vue({
             template: {
                 transformAssetUrls: {
-                    // The Vue plugin will re-write asset URLs, when referenced
-                    // in Single File Components, to point to the Laravel web
-                    // server. Setting this to `null` allows the Laravel plugin
-                    // to instead re-write asset URLs to point to the Vite
-                    // server instead.
                     base: null,
-
-                    // The Vue plugin will parse absolute URLs and treat them
-                    // as absolute paths to files on disk. Setting this to
-                    // `false` will leave absolute URLs un-touched so they can
-                    // reference assets in the public directory as expected.
                     includeAbsolute: false,
                 },
             },
@@ -34,5 +24,10 @@ export default defineConfig({
         alias: {
             vue: "vue/dist/vue.esm-bundler.js",
         },
+    },
+    server: {
+        host: true,
+        port: '5173',
+        hmr: { host: 'localhost' },
     },
 });
