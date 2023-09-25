764737732b1c80db284fcce47b0ec81ce410e5e3
fmjp1012 <fmjp1012@gmail.com>
2023-09-25 12:58:12 +0900

    一時的なgoogle controller作成

--- laravel/app/Http/Controllers/GoogleController.php
+++ laravel/app/Http/Controllers/GoogleController.php
@@ -21,34 +21,7 @@ class GoogleController extends Controller
         } catch (\Exception $e) {
             return redirect('/')->with('error', 'Google login failed');
         }
-
-        $existingGoogleAccount = GoogleAccount::where('google_id', $googleAccount->getId())->first();
-
-        // GoogleAccountがDBに存在しない場合、まずUserを新規作成
-        if (is_null($existingGoogleAccount)) {
-            $newUser = User::create(
-                ['user_name' => $googleAccount->getName()]
-            );
-
-            GoogleAccount::create([
-                'user_id' => $newUser->id,
-                'google_id' => $googleAccount->getId(),
-                'google_name' => $googleAccount->getName(),
-                'email' => $googleAccount->getEmail(),
-                'avatar' => $googleAccount->getAvatar(),
-            ]);
-
-            Auth::login($newUser);
-        } else {
-            $existingGoogleAccount->update([
-                'google_name' => $googleAccount->getName(),
-                'email' => $googleAccount->getEmail(),
-                'avatar' => $googleAccount->getAvatar(),
-            ]);
-
-            Auth::login($existingGoogleAccount->user);
-        }
-
-        return redirect('/about');
+        Auth::login();
+        return redirect('/testRedirect');
     }
 }
