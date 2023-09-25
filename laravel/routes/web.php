17703c821126c97e836112ab8d370b02ab7e25c8
fmjp1012 <fmjp1012@gmail.com>
2023-09-25 18:46:14 +0900

    google oauth 用routeを設定

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\CardController;
 use App\Http\Controllers\GoogleController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\SignupController;
+use App\Http\Controllers\TopController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,11 +20,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('welcome', function () {
     return view('welcome');
 });
+
+// 表示
 Route::get('/login', [LoginController::class, 'showAuthOptions'])->name('login');
 
+// Login
 Route::get('/login/google', [GoogleController::class, 'redirectToGoogle']);
 Route::get('/login/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 // Route::get('/logout/google')
 
 Route::middleware(['auth'])->group(function () {
+    Route::get('/', [TopController::class, 'top'])->name('top');
 });
